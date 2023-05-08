cb3df58f5764ef277d04c31c15e89b163e8b4dd0
utopia-code <utopiacode.developer@gmail.com>
2023-05-08 16:16:36 +0200

    Tasks: - add meta title article site - add meta title index site - add meta title members site - add courses site - add image to page title courses - add js to show nav-link active - add objects to scss - add card component - change color to white page title - add variables card component - add collapsed class button accordion - add cards posthtml views - add js classes in navigation header and footer - add groups posthtml views - add icon media members

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -13,8 +13,24 @@ import { Collapse as Collapse, Carousel as Carousel } from 'bootstrap';
  * Write any other JavaScript below
  */
 
-
 +( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
+
+  // add active class navigation based on url
+
+  var current = location.pathname.split('/')[1];
+  var menuItems = document.querySelectorAll('.js-nav__link');
+  
+  if (current === "") {
+      menuItems[0].classList.add('nav__link--active')
+      return;
+  }
+  // if (current === "museum.html") {
+  //     menuItems[1].classList.add('is-active')
+  //     return;
+  // }
+  for (var i = 0, l = menuItems.length; i < l; i++) {
+      if (menuItems[i].getAttribute('href').indexOf(current) !== -1) {
+          menuItems[i].classList.add('nav__link--active');
+      }
+  }
 } )();
