84c0957385c460a21495fd68aa0b727f40104d71
utopia-code <utopiacode.developer@gmail.com>
2023-05-08 18:50:11 +0200

    Tasks: - delete comments in html - set svg logo - delete carousel in js and set main item 3 - color text in hover links - delete style sections (bootstrap) - delete side padding media cards in mobile - delete code fontawesome icon logo - set color bg in page title - functions color adjust scss - delete id header accordion - set margin botton cards bootstrap

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -7,7 +7,7 @@
 // import SomeModule from 'some-node-module';
 
 
-import { Collapse as Collapse, Carousel as Carousel } from 'bootstrap';
+import { Collapse as Collapse } from 'bootstrap';
 
 /**
  * Write any other JavaScript below
@@ -21,13 +21,10 @@ import { Collapse as Collapse, Carousel as Carousel } from 'bootstrap';
   var menuItems = document.querySelectorAll('.js-nav__link');
   
   if (current === "") {
-      menuItems[0].classList.add('nav__link--active')
+      menuItems[3].classList.add('nav__link--active')
       return;
   }
-  // if (current === "museum.html") {
-  //     menuItems[1].classList.add('is-active')
-  //     return;
-  // }
+
   for (var i = 0, l = menuItems.length; i < l; i++) {
       if (menuItems[i].getAttribute('href').indexOf(current) !== -1) {
           menuItems[i].classList.add('nav__link--active');
